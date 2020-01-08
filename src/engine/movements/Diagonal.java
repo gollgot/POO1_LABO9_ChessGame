c2b4e621ee91d3513e994982e7f869632f706bc7
Loïc Dessaules <loic.dessaules@heig-vd.ch>
2020-01-08 21:39:53 +0100

    Finish clickedCell, pieceOnWay and eatablePiece check for diagonal move

--- src/engine/movements/Diagonal.java
+++ src/engine/movements/Diagonal.java
@@ -19,6 +19,7 @@ public class Diagonal implements Move {
     public boolean isValid(Cell[][] board, int fromX, int fromY, int toX, int toY, int distance, PlayerColor playerColor) {
         int horizontalGap = direction == Direction.DIAG_TOP_LEFT || direction == Direction.DIAG_BOT_LEFT ? fromX - toX : toX - fromX;
         int verticalMultiplier = direction == Direction.DIAG_TOP_LEFT || direction == Direction.DIAG_TOP_RIGHT ? 1 : -1;
+        int colorMultiplier = playerColor == PlayerColor.WHITE ? 1 : -1;
         int verticalGap = playerColor == PlayerColor.WHITE ? (toY - fromY) * verticalMultiplier : (fromY - toY) * verticalMultiplier;
         int gap = -1;
 
@@ -26,8 +27,24 @@ public class Diagonal implements Move {
             gap = horizontalGap;
         }
 
-        // Correct move
-        return gap <= distance && gap >= 0;
+        boolean isClickedCellValid = gap <= distance && gap >= 0;
+        if(!isClickedCellValid){
+            return false;
+        }
+        // Check if no piece is on the diagonal way (dont check the last)
+        else{
+            for(int i = 1; i < gap; ++i){
+                int row = direction == Direction.DIAG_TOP_LEFT || direction == Direction.DIAG_TOP_RIGHT ? fromY + (i * colorMultiplier): fromY - (i * colorMultiplier);
+                int col = direction == Direction.DIAG_TOP_LEFT || direction == Direction.DIAG_BOT_LEFT ? fromX - i : fromX + i;
+                // If the cell is not empty -> error there is a piece on the way
+                if(!board[row][col].empty()) {
+                    return false;
+                }
+            }
 
+            // Check if the cell we want to go is empty or eatable
+            Cell toCell = board[toY][toX];
+            return toCell.empty() || toCell.getPiece().getColor() != playerColor;
+        }
     }
 }
