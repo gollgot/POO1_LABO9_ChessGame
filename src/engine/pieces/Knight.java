ad234765274decb77647d248e997ffb3356c0441
Doran Kayoumi <dorankayoumi@gmail.com>
2020-01-09 15:21:07 +0100

    Implement Knight move

--- src/engine/pieces/Knight.java
+++ src/engine/pieces/Knight.java
@@ -9,15 +9,21 @@ public class Knight extends Piece {
 
     public Knight(PlayerColor color){
         super(
-                PieceType.KNIGHT,
-                color,
-                new Move[0],
-                3 // Or 1 ? a voir
+            PieceType.KNIGHT,
+            color,
+            new Move[0],
+            3
         );
     }
 
     @Override
     public boolean isValidMove(Cell[][] board, int fromX, int fromY, int toX, int toY) {
-        return false;
+
+        int yGap = fromY - toY;
+        int xGap = fromX - toX;
+
+        Cell toCell = board[toY][toX];
+        return (Math.abs(yGap) == 2 && Math.abs(xGap) == 1 || Math.abs(xGap) == 2 && Math.abs(yGap) == 1) &&
+                (toCell.empty() || toCell.getPiece().getColor() != getColor());
     }
 }
