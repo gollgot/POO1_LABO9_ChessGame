404fb8b3e0eb391455a56dd7fe73f00a4b78e5c8
Loïc Dessaules <loic.dessaules@gmail.com>
2020-01-11 20:36:18 +0100

    Add comments

--- src/engine/Cell.java
+++ src/engine/Cell.java
@@ -7,20 +7,35 @@ public class Cell {
     private int y;
     private Piece piece;
 
-
+    /**
+     * Constructor
+     * @param x The X coordinate
+     * @param y The Y coordinate
+     */
     public Cell(int x, int y) {
         this.y = y;
         this.x = x;
     }
 
+    /**
+     * Check if there is a Piece on the Cell
+     * @return True if there is a piece on the Cell, false otherwise
+     */
     public boolean empty() {
         return piece == null;
     }
 
+    /**
+     * Remove the Piece on the cell
+     */
     public void removePiece() {
         piece = null;
     }
 
+    /**
+     * Add a piece on the Cell
+     * @param p The Piece that will be add on the Cell
+     */
     public void addPiece(Piece p) {
         piece = p;
         piece.setCell(this);
